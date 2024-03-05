6102e1b1574ac578ae1b27fd5f88114cedb96447
youwol-guillaume <greinisch@youwol.com>
2024-03-05 16:43:31 +0100

    🔖 start `0.2.0-wip`

--- src/auto-generated.ts
+++ src/auto-generated.ts
@@ -118,13 +118,13 @@ const entries = {
 export const setup = {
     name:'@youwol/co-lab',
         assetId:'QHlvdXdvbC9jby1sYWI=',
-    version:'0.1.6-wip',
+    version:'0.2.0-wip',
     shortDescription:"The YouWol's collaborative laboratory application.",
     developerDocumentation:'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/co-lab&tab=doc',
     npmPackage:'https://www.npmjs.com/package/@youwol/co-lab',
     sourceGithub:'https://github.com/youwol/co-lab',
     userGuide:'https://l.youwol.com/doc/@youwol/co-lab',
-    apiVersion:'01',
+    apiVersion:'02',
     runTimeDependencies,
     externals,
     exportedSymbols,
@@ -149,7 +149,7 @@ export const setup = {
             modules,
             scripts,
         }).then(() => {
-            return window[`@youwol/co-lab_APIv01`]
+            return window[`@youwol/co-lab_APIv02`]
         })
     },
     installAuxiliaryModule: ({name, cdnClient, installParameters}:{
@@ -164,7 +164,7 @@ export const setup = {
         const parameters = installParameters || {}
         const scripts = [
             ...(parameters.scripts || []),
-            `@youwol/co-lab#0.1.6-wip~dist/@youwol/co-lab/${entry.name}.js`
+            `@youwol/co-lab#0.2.0-wip~dist/@youwol/co-lab/${entry.name}.js`
         ]
         const modules = [
             ...(parameters.modules || []),
@@ -175,7 +175,7 @@ export const setup = {
             modules,
             scripts,
         }).then(() => {
-            return window[`@youwol/co-lab/${entry.name}_APIv01`]
+            return window[`@youwol/co-lab/${entry.name}_APIv02`]
         })
     },
     getCdnDependencies(name?: string){
