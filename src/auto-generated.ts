6313b051f6d8576a2807d9e09abc595f0ad1a8f0
youwol-guillaume <greinisch@youwol.com>
2024-03-28 15:45:30 +0100

    🔖 start `0.3.3-wip`

--- src/auto-generated.ts
+++ src/auto-generated.ts
@@ -126,7 +126,7 @@ const entries = {
 export const setup = {
     name:'@youwol/co-lab',
         assetId:'QHlvdXdvbC9jby1sYWI=',
-    version:'0.3.2',
+    version:'0.3.3-wip',
     shortDescription:"The YouWol's collaborative laboratory application.",
     developerDocumentation:'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/co-lab&tab=doc',
     npmPackage:'https://www.npmjs.com/package/@youwol/co-lab',
@@ -172,7 +172,7 @@ export const setup = {
         const parameters = installParameters || {}
         const scripts = [
             ...(parameters.scripts || []),
-            `@youwol/co-lab#0.3.2~dist/@youwol/co-lab/${entry.name}.js`
+            `@youwol/co-lab#0.3.3-wip~dist/@youwol/co-lab/${entry.name}.js`
         ]
         const modules = [
             ...(parameters.modules || []),
