86f4c8bed07ff8eef304d17bfa028280771b3d75
youwol-guillaume <greinisch@youwol.com>
2024-05-29 09:20:24 +0200

    💄 `yw_pipeline.py` => set application's graphics

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -1,7 +1,16 @@
+from pathlib import Path
+
 from youwol.app.environment import YouwolEnvironment
 from youwol.app.routers.projects import IPipelineFactory, BrowserApp, Execution, Link, BrowserAppGraphics
 from youwol.pipelines.pipeline_typescript_weback_npm import pipeline, PipelineConfig, PublishConfig
+from youwol.utils import parse_json, encode_id
 from youwol.utils.context import Context
+
+
+folder_path = Path(__file__).parent.parent
+pkg_json = parse_json(folder_path / "package.json")
+asset_id = encode_id(pkg_json['name'])
+version = pkg_json['version']
 
 
 class PipelineFactory(IPipelineFactory):
@@ -11,12 +20,12 @@ class PipelineFactory(IPipelineFactory):
 
     async def get(self, _env: YouwolEnvironment, context: Context):
         config = PipelineConfig(target=BrowserApp(
-            displayName="py-youwol-doc",
+            displayName="Co-Lab",
             execution=Execution(
                 standalone=True
             ),
             graphics=BrowserAppGraphics(
-                appIcon={'class': 'far fa-laugh-beam fa-2x'},
+                appIcon=icon(size_px='100%', border_radius='15%', icon_path=app_icon),
                 fileIcon={}
             ),
             links=[
@@ -26,3 +35,21 @@ class PipelineFactory(IPipelineFactory):
             ]
         ), publishConfig=PublishConfig(packagedFolders=["assets"]))
         return await pipeline(config, context)
+
+
+assets_dir = f"/api/assets-gateway/cdn-backend/resources/{asset_id}/{version}/assets"
+app_icon = f"url('{assets_dir}/co-lab-light.png')"
+
+
+def icon(size_px: str, border_radius: str, icon_path: str, bg_size: str = "cover"):
+    return {
+        "style": {
+            "width": f"{size_px}",
+            "height": f"{size_px}",
+            "background-image": icon_path,
+            "background-size": bg_size,
+            "background-repeat": "no-repeat",
+            "background-position": "center center",
+            "border-radius": f"{border_radius}",
+        }
+    }
