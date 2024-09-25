769c123863709c18058c0937bf1debb3c408ddf1
youwol-guillaume <greinisch@youwol.com>
2024-09-25 18:15:02 +0200

    🥅 [Projects] fix case of undefined project's icon

--- src/app/projects/icons.ts
+++ src/app/projects/icons.ts
@@ -1,7 +1,8 @@
 import { Routers } from '@youwol/local-youwol-client'
 import { NavIconSvg } from '../common'
+import { AnyVirtualDOM } from '@youwol/rx-vdom'
 
-export function icon(project: Routers.Projects.Project) {
+export function icon(project: Routers.Projects.Project): AnyVirtualDOM {
     const filenames = {
         typescript: 'icon-TS.svg',
         python: 'icon-python.svg',
@@ -16,6 +17,9 @@ export function icon(project: Routers.Projects.Project) {
     }
     if (project.pipeline.tags.includes('javascript')) {
         filename = filenames.javascript
+    }
+    if (filename === '') {
+        return { tag: 'div' }
     }
     return new NavIconSvg({
         filename,
